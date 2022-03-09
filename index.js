5965abb676be343303fb78acafce75de3e718952
Audrey Schmidt <audsch@outlook.com>
2022-03-09 11:18:43 -0600

    feat: sample REST api with node + express.js with CRUD functionality

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { update } = require('./repository/repository-base');
+const repoContext = require('./repository/repository-wrapper');
+const { validateProduct } = require('./middleware/product-validation');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
+
+app.listen(3000, function() {
+    console.log('Server started. Listening on port 3000.');
+});
+
+app.get('/api/products', (req, res) => {
+    const products = repoContext.products.findAllProducts();
+    return res.send(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const id = req.params.id;
+    const product = repoContext.products.findProductById(id);
+    return res.send(product);
+});
+
+app.post('/api/products', [validateProduct], (req, res) => {
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return res.send(addedProduct);
+});
+
+app.put('/api/products/:id', [validateProduct], (req, res) => {
+    const id = req.params.id;
+    const productPropertiesToUpdate = req.body;
+    const updatedProduct = repoContext.products.updateProduct(id, productPropertiesToUpdate);
+    return res.send(updatedProduct);
+});
+
+app.delete('/api/products/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.products.deleteProduct(id);
+    return res.send(updatedDataSet);
+});
